eeb3f0d7d79bcac99ad48358ddfff7fee573d692
Emmanuel Thompson <eet6646@gmail.com>
2022-10-17 09:21:04 -0400

    use hrsh7th's version
    https://github.com/hrsh7th/cmp-vsnip/pull/4 is now merged

--- neovim-init-lsp-cmp-rust-tools.lua
+++ neovim-init-lsp-cmp-rust-tools.lua
@@ -39,7 +39,7 @@ require("packer").startup(function(use)
     -- cmp LSP completion
     "hrsh7th/cmp-nvim-lsp",
     -- cmp Snippet completion
-    "sharksforarms/cmp-vsnip",
+    "hrsh7th/cmp-vsnip",
     -- cmp Path completion
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-buffer",
