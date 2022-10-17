aa706aa9c5a57032aa48f42ee0f49e6add1559cb
Emmanuel Thompson <eet6646@gmail.com>
2022-10-17 09:14:56 -0400

    move non-lua configs to `unmaintained`
    lua is the future

--- run.sh
+++ run.sh
@@ -4,14 +4,21 @@ sudo docker build -t vim-rust .
 
 CONFIG_FILE="$1"
 
-if [[ `basename $CONFIG_FILE` == neovim* ]]
+if [[ `basename $CONFIG_FILE` == neovim*.lua ]]
 then
     EDITOR_BIN="nvim"
     MOUNT_PATH="/root/.config/nvim/init.lua"
+    PLUGIN_SYS="packer"
+elif [[ `basename $CONFIG_FILE` == neovim*.vim ]]
+then
+    EDITOR_BIN="nvim"
+    MOUNT_PATH="/root/.config/nvim/init.vim"
+    PLUGIN_SYS="plug"
 elif [[ `basename $CONFIG_FILE` == vim* ]]
 then
     EDITOR_BIN="vim"
     MOUNT_PATH="/root/.vimrc"
+    PLUGIN_SYS="plug"
 else
     echo "Does not start with 'neovim' or 'vim' cannot select editor"
 fi
@@ -21,12 +28,12 @@ CONFIG_FILE=`readlink -f $CONFIG_FILE`
 echo "Editor: $EDITOR_BIN"
 echo "Mount: $CONFIG_FILE -> $MOUNT_PATH"
 
-if [[ `basename $CONFIG_FILE` == neovim* ]]
+if [[ $PLUGIN_SYS == "packer" ]]
 then
     sudo docker run -v $CONFIG_FILE:$MOUNT_PATH -it vim-rust /bin/bash -c "cd ~/test_app && nvim --headless -c 'sleep 5' +qa && /bin/bash"
-elif [[ `basename $CONFIG_FILE` == vim* ]]
+elif [[ $PLUGIN_SYS == "plug" ]]
 then
     sudo docker run -v $CONFIG_FILE:$MOUNT_PATH -it vim-rust /bin/bash -c "cd ~/test_app && $EDITOR_BIN -E -s -u NONE $MOUNT_PATH +so +PlugInstall +qa || /bin/bash"
 else
-    echo "Does not start with 'neovim' or 'vim' cannot select editor"
+    echo "Error: Could not determine how to run."
 fi
